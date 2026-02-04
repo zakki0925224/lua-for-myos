a511eaa9e8cdab90c0c9d8c915c8db356c8bc583
zakki0925224 <zakki0925224@gmail.com>
2026-02-05 00:20:58 +0900

    [Update]: Enabled other standard libraries without math

--- linit.c
+++ linit.c
@@ -21,15 +21,15 @@
 */
 static const luaL_Reg stdlibs[] = {
     {LUA_GNAME, luaopen_base},
-    // {LUA_LOADLIBNAME, luaopen_package},
-    // {LUA_COLIBNAME, luaopen_coroutine},
-    // {LUA_DBLIBNAME, luaopen_debug},
-    // {LUA_IOLIBNAME, luaopen_io},
+    {LUA_LOADLIBNAME, luaopen_package},
+    {LUA_COLIBNAME, luaopen_coroutine},
+    {LUA_DBLIBNAME, luaopen_debug},
+    {LUA_IOLIBNAME, luaopen_io},
     // {LUA_MATHLIBNAME, luaopen_math},
-    // {LUA_OSLIBNAME, luaopen_os},
-    // {LUA_STRLIBNAME, luaopen_string},
-    // {LUA_TABLIBNAME, luaopen_table},
-    // {LUA_UTF8LIBNAME, luaopen_utf8},
+    {LUA_OSLIBNAME, luaopen_os},
+    {LUA_STRLIBNAME, luaopen_string},
+    {LUA_TABLIBNAME, luaopen_table},
+    {LUA_UTF8LIBNAME, luaopen_utf8},
     {NULL, NULL}};
 
 /*
